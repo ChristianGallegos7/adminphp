3fd747205c4859c2e5a65b7a0b092c753343520e
Christian <christiantenz7@gmail.com>
2023-08-09 14:17:16 -0500

    colores

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
-<?php 
-$url_base="http://localhost/dashboard/admin/";
+<?php
+$url_base = "http://localhost/dashboard/admin/";
 // $url_base="http://localhost/admin/";
 ?>
 
@@ -13,8 +13,7 @@ $url_base="http://localhost/dashboard/admin/";
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
 </head>
 
@@ -22,13 +21,13 @@ $url_base="http://localhost/dashboard/admin/";
   <header>
     <!-- place navbar here -->
     <nav class="navbar navbar-expand navbar-light bg-light">
-        <div class="nav navbar-nav">
-            <a class="nav-item nav-link active" href="#" aria-current="page">Sistema<span class="visually-hidden">(current)</span></a>
-            <a class="nav-item nav-link" href="<?php echo $url_base;?>sections/empleados/">Empleados</a>
-            <a class="nav-item nav-link" href="<?php echo $url_base;?>sections/puestos/">Puestos</a>
-            <a class="nav-item nav-link" href="<?php echo $url_base;?>sections/usuarios/">Usuarios</a>
-            <a class="nav-item nav-link" href="#">Cerrar Sesion</a>
-        </div>
+      <div class="nav navbar-nav">
+        <a class="nav-item nav-link active" href="#" aria-current="page">Sistema<span class="visually-hidden">(current)</span></a>
+        <a class="nav-item nav-link btn btn-info text-white" href="<?php echo $url_base; ?>sections/empleados/">Empleados</a>
+        <a class="nav-item nav-link btn btn-primary mx-3 text-white" href="<?php echo $url_base; ?>sections/puestos/">Puestos</a>
+        <a class="nav-item nav-link btn btn-warning text-white" href="<?php echo $url_base; ?>sections/usuarios/">Usuarios</a>
+        <a class="nav-item nav-link btn btn-danger text-white mx-3" href="#">Cerrar Sesion</a>
+      </div>
     </nav>
   </header>
   <main class="container">
