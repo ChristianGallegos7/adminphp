040673473f549dac95f805bd842f1cce8800612d
Christian <christiantenz7@gmail.com>
2023-08-09 20:07:30 -0500

    l

--- sections/empleados/index.php
+++ sections/empleados/index.php
@@ -1,4 +1,14 @@
-<?php include("../../templates/header.php")?>
+<?php
+include("../../bd.php");
+
+
+//listar los empleados
+$sentencia = $conexion->prepare("SELECT * FROM tbl_empleados");
+$sentencia->execute();
+$lista_tbl_empleados = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php include("../../templates/header.php") ?>
 <br>
 <h4>Empleados</h4>
 <div class="card">
@@ -11,6 +21,7 @@
             <table class="table table-info">
                 <thead>
                     <tr>
+                        <th scope="col">Id</th>
                         <th scope="col">Nombre</th>
                         <th scope="col">Foto</th>
                         <th scope="col">CV</th>
@@ -20,26 +31,36 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="">
-                        <td scope="row">Christian</td>
-                        <td>Imagen.jpg</td>
-                        <td>Cv.pdf</td>
-                        <td>Programador jr</td>
-                        <td>12/12/12</td>
-                        <td>
-                            <a name="" id="" class="btn btn-primary" href="#" role="button">Carta</a>
-                            <a name="" id="" class="btn btn-success" href="#" role="button">Editar</a>
-                            <a name="" id="" class="btn btn-danger" href="#" role="button">Eliminar</a>
-                        </td>
 
-                    </tr>
-                   
+                    <?php foreach ($lista_tbl_empleados as $registro) { ?>
+
+                        <tr class="">
+                            <td scope="row"><?php echo $registro['id'] ?></td>
+                            <td>
+                                <?php echo $registro['primernombre'] ?>
+                                <?php echo $registro['segundonombre'] ?>
+                                <?php echo $registro['primerapellido'] ?>
+                                <?php echo $registro['segundoapellido'] ?>
+                            </td>
+                            <td><?php echo $registro['foto'] ?></td>
+                            <td><?php echo $registro['cv'] ?></td>
+                            <td>Programador jr</td>
+                            <td>12/12/12</td>
+                            <td>
+                                <a name="" id="" class="btn btn-primary" href="#" role="button">Carta</a>
+                                <a name="" id="" class="btn btn-success" href="#" role="button">Editar</a>
+                                <a name="" id="" class="btn btn-danger" href="#" role="button">Eliminar</a>
+                            </td>
+
+                        </tr>
+                    <?php } ?>
+
                 </tbody>
             </table>
         </div>
-        
+
     </div>
-    
+
 </div>
 
-<?php include("../../templates/footer.php")?>
+<?php include("../../templates/footer.php") ?>
